af66411ae3d6a358b63a22cc141870a688e6d9fb
TAOSAND <taofeekdboy@yahoo.com>
2021-10-31 15:33:41 +0200

    aws updated

--- sequelize folder/sequelize.js
+++ sequelize folder/sequelize.js
@@ -9,34 +9,18 @@ const port = process.env.PORT;
 
 let sequelize;
 
-// if (env === "development") {
-//   sequelize = new Sequelize("lithuania", "postgres", "police", {
-//     host: "localhost",
-//     dialect: "postgres",
-//     logging: false,
-//   });
-// } else {
-//   // sequelize = new Sequelize(database, username, password, {
-//   //   host: host,
-//   //   dialect: "postgres",
-//   //   logging: false,
-//   // });
-//   sequelize = new Sequelize(
-//     `postgres:${username}:${password}@${host}:5432/${database}`
-//   );
-// }
-if (process.env.NODE_ENVIRONMENT === "development") {
-  sequelize = new Sequelize("lithuania", "postgres", "police", {
-    host: "localhost",
-    dialect: "postgres",
-    logging: false,
-  });
-}
-
 if (process.env.PRODUCTION_ENVIRONMENT === "production") {
   sequelize = new Sequelize(
     `postgres:${username}:${password}@${host}:5432/${database}`
   );
+} else {
+  if (process.env.NODE_ENVIRONMENT === "development") {
+    sequelize = new Sequelize("lithuania", "postgres", "police", {
+      host: "localhost",
+      dialect: "postgres",
+      logging: false,
+    });
+  }
 }
 
 module.exports = sequelize;
