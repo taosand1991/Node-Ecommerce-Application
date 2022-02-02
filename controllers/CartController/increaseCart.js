74790c0e93e1269a49d4ec12019ee9fdc18bf87f
Adesina Taofeek <taofeekdboy@yahoo.com>
2022-02-02 17:02:25 +0200

    Update increaseCart.js

--- controllers/CartController/increaseCart.js
+++ controllers/CartController/increaseCart.js
@@ -1,5 +1,4 @@
 const CartItem = require("../../models/CartItem");
-const Product = require("../../models/Product");
 const jwt = require("jsonwebtoken");
 
 module.exports = async function (req, res) {
